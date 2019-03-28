d4a8d9da542bd7fe905744c1f48c55de2dcc67f4
Kodey Converse <kconverse@128technology.com>
2019-03-28 10:25:37 -0400

    Fire

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = 80
 
 const requestHandler = (request, response) => {
   console.log(request.url)
-  response.end('Hello World!')
+  response.end('<iframe style="position: absolute; top: 0; left: 0;" src="https://www.youtube.com/embed/ZY3J3Y_OU0w?controls=0&amp;theme=dark&amp;autoplay=1&amp;autohide=1&amp;modestbranding=1&amp;fs=0&amp;showinfo=0&amp;rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="" width="100%" height="100%"></iframe>')
 }
 
 const server = http.createServer(requestHandler)
